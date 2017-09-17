dd85501564cd1a7d23071b735fe6493d795dc346
caporta <caporta@users.noreply.github.com>
2017-09-17 14:02:38 -0400

    Use url masking to expose slug instead of encoded query string on client-side requests

--- pages/index.js
+++ pages/index.js
@@ -7,9 +7,9 @@ const Index = () => (
   <section>
     <h1>My Blog</h1>
     <ul>
-      <PostLink title='Hello Next.js'/>
-      <PostLink title='Learn Next.js is awesome'/>
-      <PostLink title='Deploy apps with Zeit'/>
+      <PostLink slug='hello-nextjs' title='Hello Next.js'/>
+      <PostLink slug='learn-nextjs' title='Learn Next.js is awesome'/>
+      <PostLink slug='deploy-nextjs' title='Deploy apps with Zeit'/>
     </ul>
   </section>
 )
